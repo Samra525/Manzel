321b9ac34eb8d848934731aa82185ed8fffba983
Samra <mrsawais88088@gmail.com>
2021-05-28 21:01:27 +0500

    contact form redirection to home page

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,27 +1,62 @@
 <template>
-  
+
     <form
     id="contact-form" 
     method="POST"
-    action="https://docs.google.com/forms/u/0/d/e/
-    1FAIpQLSe_YJ0Ixq91chVXksMZsC7ULHPMThgXI19-Or81LsFCnVs7nQ/formResponse">
+    action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSe_YJ0Ixq91chVXksMZsC7ULHPMThgXI19-Or81LsFCnVs7nQ/formResponse">
 
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe_YJ0Ixq91chVXksMZsC7ULHPMThgXI19-Or81LsFCnVs7nQ/viewform?embedded=true" width="640" 
-    height="1078" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading…</iframe>
+    <!-- <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe_YJ0Ixq91chVXksMZsC7ULHPMThgXI19-Or81LsFCnVs7nQ/viewform?embedded=true" width="640" 
+    height="1078" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading…</iframe> -->
 
-<!-- <label>Name</label>
-          <input name="entry.2005620554" type="text" />
+<div class="w3-section">
+            <label>Name *</label>
+            <input type="text" name="entry.2005620554" placeholder="Please enter your name"/>
+        </div>
+        <div>
+            <label>Email *</label>
+            <input type="email" name="entry.1045781291"  placeholder="Please enter your email address" required/>
+        </div>
+        <div>
+          <label>Message *</label>
+            <textarea name="entry.2037741915"  rows="5" placeholder="Please enter your message" ></textarea>
+        </div>
 
-          <label>Email</label>
-          <input name="entry.1045781291" type="email" required />
+<div>
+            <label>Address</label>
+            <textarea input type="text" rows="4" name="entry.1065046570"  placeholder="Please enter your address" required/>
+        </div>
 
-<div class="form-group">
+
+<div>
+            <label>Phone number</label>
+            <input type="numbers" name="entry.1166974658" placeholder="Please enter your phone number" required/>
+        </div>
+
+<div>
+            <label>Comments</label>
+            <textarea input type="text" rows="5"  name="entry.839337160"  placeholder="Please enter your comments" required/>
+        </div>
+
+
+
+
+
+        <div class="form-group">
  <button class="btn btn-raised btn-block btn-danger" type="submit" value="submit">Submit →</button>
-						 	</div> -->
+						 	</div>
 
 
+
+               <!-- <div id="error_message" style="width:100%; height:100%; display:none; ">
+						<h4>Error</h4>
+						Sorry there was an error sending your form.
+						</div>
+						<div id="success_message" style="width:100%; height:100%; display:none; ">
+						<h2>Success! Your Message was Sent Successfully.</h2>
+						</div> -->
 
     </form>
+
 </template>
 
 <script>
